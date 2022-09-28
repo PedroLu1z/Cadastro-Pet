852598a0593f4ec21d6b3d1835b5a432863b4265
brunobrasilll <RM93263@fiap.com.br>
2022-09-27 21:32:31 -0300

    Algumas Alterações nos cards do PetInfo.

--- react-app-pet/src/components/PetInfo/PetInfo.jsx
+++ react-app-pet/src/components/PetInfo/PetInfo.jsx
@@ -1,33 +1,40 @@
-import React from 'react'
-import { Link } from 'react-router-dom';
-import './details.css'
-import PetBg from '../CadastroPet/img/pet-bg2.jpg';
+import React from "react";
+import { Link } from "react-router-dom";
+import "./details.css";
+import PetBg from "../CadastroPet/img/pet-bg2.jpg";
 
 const PetInfo = () => {
-    return(
-        <div class="card-container">
-        <div class="img-pet">
-            <img src={PetBg} alt="foto do pet"/>
-            
-        </div>
-        <div class="nome-pet">
-            <h3>Nome Pet</h3>
-        </div>
-        <div class="pet-info">
-            <p>Raça:</p>
-            <p>Data-nascimento:</p>
-            <p>Tamanho:</p>
-            <p>NomeDono:</p>
-            <p>TelefoneDono:</p>
-            <textarea name="obs" id="obsPet" cols="30" rows="10" placeholder="Observações: "></textarea>
-        </div>
-        <div class="edit-btn">
-            <button>
-                Alterar Informações
-            </button>
-        </div>
+  function Mostrar() {
+    console.log("RECEBA");
+  }
+
+  return (
+    <div class="card-container">
+      <div class="img-pet">
+        <img src={PetBg} alt="foto do pet" />
+      </div>
+      <div class="nome-pet">
+        <h3>Nome Pet</h3>
+      </div>
+      <div class="pet-info">
+        <p>Raça:</p>
+        <p>Data-nascimento:</p>
+        <p>Tamanho:</p>
+        <p>NomeDono:</p>
+        <p>TelefoneDono:</p>
+        <textarea
+          name="obs"
+          id="obsPet"
+          cols="30"
+          rows="10"
+          placeholder="Observações: "
+        ></textarea>
+      </div>
+      <div class="edit-btn">
+        <button onClick={Mostrar}>Alterar Informações</button>
+      </div>
     </div>
-    )
-}
+  );
+};
 
 export default PetInfo;
